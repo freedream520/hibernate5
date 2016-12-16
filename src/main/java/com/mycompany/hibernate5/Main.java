eb95a966538d36354d7dc60209750a0250e9e8ae
Peter Kipping <pkipping@yahoo.com>
2016-12-15 17:32:59 -0800

    added JPA domain objects

--- src/main/java/com/mycompany/hibernate5/Main.java
+++ src/main/java/com/mycompany/hibernate5/Main.java
@@ -5,18 +5,23 @@
  */
 package com.mycompany.hibernate5;
 
+import java.util.logging.Logger;
+
 /**
  *
  * @author pkipping
  */
 public class Main {
 
+	private static final Logger lg = Logger.getLogger(Main.class.getName());
+	
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello World!");
+		lg.info("Testing logger");
     }
     
 }
