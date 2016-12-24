0d3f31b34f57da08d3f898de352c2dc64b14cb40
Peter Kipping <pkipping@yahoo.com>
2016-12-23 19:19:45 -0800

    testing hibernate native queries

--- src/main/java/com/mycompany/hibernate5/Main.java
+++ src/main/java/com/mycompany/hibernate5/Main.java
@@ -22,56 +22,113 @@ import javax.persistence.Persistence;
 public class Main {
 
 	private static final Logger lg = Logger.getLogger(Main.class.getName());
-	
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-        System.out.println("Hello World!");
+
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args) {
+		// TODO code application logic here
+		System.out.println("Hello World!");
 		lg.info("Testing logger");
-		
+
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("sakila");
+
+		try {
+			nativeQuery(emf);
+			testJPA(emf);
+		} finally {
+			if (emf != null && emf.isOpen()) {
+				emf.close();
+			}
+			lg.info("Closing EM Factory");
+		}
+	}
+
+	public static void nativeQuery(EntityManagerFactory emf) {
+
+		EntityManager em = emf.createEntityManager();
+		try {
+//			emf = Persistence.createEntityManagerFactory("sakila");
+//			em = emf.createEntityManager();
+			String q = "select c from Customer c WHERE c.lastName = :lastName";
+			em.getTransaction().begin();
+			List<Customer> list = em.createQuery(q, Customer.class)
+				.setParameter("lastName", "Vest")
+				//				.setHint( "org.hibernate.readOnly", true )
+				.getResultList();
+
+			for (Customer c : list) {
+				lg.info("Customer Name: " + c.getFirstName() + " " + c.getLastName());
+				c.setFirstName("JULIAN");
+				lg.info("Address: " + c.getAddressId().getAddress());
+			}
+
+			//Native queries
+			List<Object[]> cust1 = em.createNativeQuery(
+				"SELECT * FROM customer LIMIT 5")
+				.getResultList();
+
+			for (Object[] c : cust1) {
+				lg.info("Name: " + c[2]);
+			}
+
+			//Entity Queries
+			List<Customer> cust2 = em.createNativeQuery(
+				"SELECT * FROM customer LIMIT 5", Customer.class)
+				.getResultList();
+
+			em.getTransaction().commit();
+			em.close();
+
+		} catch (Exception e) {
+			lg.log(Level.SEVERE, e.getMessage(), e);
+//			e.printStackTrace();
+		} finally {
+			if (em != null && em.isOpen()) {
+				em.close();
+			}
+		}
+	}
+
+	public static void testJPA(EntityManagerFactory emf) {
 		//Testing JPA
-        EntityManagerFactory factory = null;
-        EntityManager em = null;
-        try {
-            factory = Persistence.createEntityManagerFactory("sakila");
-            em = factory.createEntityManager();
 
-            //do queries
-            String ql = "select c from Customer c "
-                    + "join fetch c.rentalList r "
-//                    + "join fetch c.paymentList p "
-                    + "where c.lastName = :lastName";
-            List<Customer> list = em.createQuery(ql).setParameter("lastName", "Vest")
-                    .setMaxResults(10).getResultList();
+		EntityManager em = null;
+		try {
+			em = emf.createEntityManager();
 
-            for (Customer c : list) {
-                lg.log(Level.INFO, "Customer Name {0}", c.getLastName());
-                int i = 0;
-                for (Rental r: c.getRentalList()) {
-                    lg.log(Level.INFO, "Rental ID: {0} rental date {1}", new Object[]{r.getRentalId(), r.getRentalDate()});
-                    ++i;
-                    lg.info("Int i: " + i);
-                    if (i>4) break;
-                }
-                for (Payment p : c.getPaymentList()) {
-                    lg.info("Payment id: " + p.getPaymentId() + " Amount: " + p.getAmount());
-                }
-            }
+			//do queries
+			String ql = "select c from Customer c "
+				+ "join fetch c.rentalList r "
+				//                    + "join fetch c.paymentList p "
+				+ "where c.lastName = :lastName";
+			List<Customer> list = em.createQuery(ql).setParameter("lastName", "Vest")
+				.setMaxResults(10).getResultList();
 
-        } catch (Exception e) {
-            lg.log(Level.SEVERE, e.getMessage(), e);
-            e.printStackTrace();
-        } finally {
-            if (em != null) {
-                em.close();
-            }
-            if (factory != null) {
-                factory.close();
-            }
-        }
-		
-    }
-    
+			for (Customer c : list) {
+				lg.log(Level.INFO, "Customer Name {0}", c.getLastName());
+				int i = 0;
+				for (Rental r : c.getRentalList()) {
+					lg.log(Level.INFO, "Rental ID: {0} rental date {1}", new Object[]{r.getRentalId(), r.getRentalDate()});
+					++i;
+					lg.info("Int i: " + i);
+					if (i > 4) {
+						break;
+					}
+				}
+				for (Payment p : c.getPaymentList()) {
+					lg.log(Level.INFO, "Payment id: {0} Amount: {1}", new Object[]{p.getPaymentId(), p.getAmount()});
+				}
+			}
+
+		} catch (Exception e) {
+			lg.log(Level.SEVERE, e.getMessage(), e);
+			e.printStackTrace();
+		} finally {
+			if (em != null) {
+				em.close();
+			}
+		}
+	}
+
 }
